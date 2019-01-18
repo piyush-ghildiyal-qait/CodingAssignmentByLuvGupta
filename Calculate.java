dd133189aec18bb44382c3715633ba895c7df88a
piyush-ghildiyal-qait <46739394+piyush-ghildiyal-qait@users.noreply.github.com>
2019-01-18 12:14:49 +0530

    Update Calculate.java

--- Calculate.java
+++ Calculate.java
@@ -4,46 +4,28 @@ public class Calculate {
 	
 	Scanner sc = new Scanner(System.in);
 	
-	public void getAreaCircle() {
+	public double getAreaCircle(double radius) {
 		
-                System.out.println("Enter the Radius in centimeter:");
-		double radius = sc.nextDouble();
-		double area = 3.14*radius*radius;
-		System.out.println("The area of circle is:"+area+" square centimeters.");
-		
-		
+                double area = 3.14*radius*radius;
+		return area;	
 	}
 	
-	public void getAreaRectangle() {
-		System.out.println("Enter Length in centimeters:");
-		double length = sc.nextDouble();
-		System.out.println("Enter Breadth in centimeters:");
-		double breadth = sc.nextDouble();
+	public double getAreaRectangle(double length,double breadth) {
+		
 		double area = length*breadth;
-		System.out.println("The area of reactangle is:"+area+" square centimeters.");
-		
+		return area;
 	}
 	
-	public void getAreaSquare() {
+	public double getAreaSquare(double side) {
 		
-		System.out.println("Enter Side in centimeters:");
-		double side = sc.nextDouble();
 		double area = side*side;
-		System.out.println("The area of square is:"+area+" square centimeters.");
-		
+       		return area;		
 	}
 	
-	public void getAreaTriangle() {
-		System.out.println("Enter Base Length in centimeters:");
-		double base = sc.nextDouble();
-		System.out.println("Enter Height in centimeters:");
-		double height = sc.nextDouble();
+	public double getAreaTriangle(double base,double height) {
+		
 		double area = (1.0/2.0)*base*height;
-		System.out.println("The area of triangle is:"+area+" square centimeters.");
-		
-		
+		return area;
 	}
 	
-	
-
 }
